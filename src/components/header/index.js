65bd754e4eeb1de8b6b63a25d8b08899ec283c24
Dale Kittendorf <drkittendorf@gmail.com>
2023-03-07 12:51:08 -0600

    recommit

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,23 +5,19 @@ import "./header.css"
 function Header() {
 return (
 <div className='header'>
-    <div className='lside'>
+     
 
-    </div>
-
-    <div className='head'>
+    
         <div>
             <h1>Chef Jake Williams</h1>
         </div>
         <div>
             <h2> Detroit, Michigan </h2>
         </div>
-    </div>
-
-    <div className= 'rside'> 
+  
 
     </div>
-</div>
+
 )}
 
 export default Header; 
