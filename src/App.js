eb563a9d5c57ad54b0e54eea25fcfcb68156234f
Dale Kittendorf <drkittendorf@gmail.com>
2023-03-07 13:30:42 -0600

    recommit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter} from 'react-router-dom';
+import BrowserRouter from 'react-router-dom';
 import Header from './components/header';
 
 function App() {
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,22 +1,14 @@
 // HEADER INDEX
 import React from "react";
-import "./header.css"
+
 
 function Header() {
 return (
-<div className='header'>
-     
-
-    
-        <div>
+            <div>
             <h1>Chef Jake Williams</h1>
-        </div>
-        <div>
+       
             <h2> Detroit, Michigan </h2>
-        </div>
-  
-
-    </div>
+            </div>
 
 )}
 
