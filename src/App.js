7a34d8df60a5d0bab21f147f6270a0690c443132
Dale Kittendorf <drkittendorf@gmail.com>
2023-03-07 11:25:48 -0600

    recommit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React and more Jake Williams
-        </a>
-        <p>SHOW ME THIS AT LEAST</p>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Header />
+    
+    </BrowserRouter>      
   );
 }
 
